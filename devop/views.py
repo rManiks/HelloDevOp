f82319098ea9664ef68aa615c479d5e057d7cad9
Mani <r.manikandan@gmail.com>
2017-06-15 23:00:19 +0100

    DevOp Testing

--- devop/views.py
+++ devop/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello DevOps World, I am a Python app! Through DevOp now")
+	return HttpResponse("Hello DevOps World, I am a Python app! Lets try a auto deploy")
