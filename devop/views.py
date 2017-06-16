ee94907f5870d4c0ed718005a4d88677d53767bd
Ramachandran <mrmc@UKLOMACRDOL.local>
2017-06-16 10:58:28 +0100

    updated from mac

--- devop/views.py
+++ devop/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("<h1>Hello DevOps World, I am a Python app! Lets try a auto deploy<h1>")
+	return HttpResponse("<h1>This is a completely new content</h1>")
