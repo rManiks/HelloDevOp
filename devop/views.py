fbd0874ab439c4a7ca10c6302369bc0fee2b9207
Ramachandran <mrmc@UKLOMACRDOL.local>
2017-06-16 08:23:29 +0100

    Included header tags

--- devop/views.py
+++ devop/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello DevOps World, I am a Python app! Lets try a auto deploy")
+	return HttpResponse("<h1>Hello DevOps World, I am a Python app! Lets try a auto deploy<h1>")
